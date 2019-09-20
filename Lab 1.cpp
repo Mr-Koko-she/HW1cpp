88052442d85e02f1ec4a2f03274dad0c36665ca0
Mr-Koko-she <55578798+Mr-Koko-she@users.noreply.github.com>
2019-09-20 03:38:47 -0500

    Add files via upload

---
+++ Lab 1.cpp
@@ -0,0 +1,68 @@
+/*  Homework Lab 1
+
+Demonstrate char and string types used with output statements.
+
+Use the cout object and insertion operator << to print different
+character values and string values to the output window.
+
+Use double quotes " and " to print a string value, and
+use single quotes ' and ' to print a character value.
+
+Example:
+
+cout << "A string";
+cout << 'A';
+
+Insert the endl (end line) manipulator into the output statement
+to start a new line or print a blank line.
+
+Example:
+
+cout << "A string" << endl;
+cout << 'A' << endl;
+cout << endl;
+
+See the "Sample Program Output" at the bottom of the listing to see
+what your program should produce when you run it.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	/*  TODO - Create a program that produces the Sample Output shown below.
+
+	You MUST:
+	- Output all of the lines show below in the Sample Output - spacing, spelling, punctuation, etc. must be exact matches
+	- Output the three lines that contain C, +, and + as char values, NOT as string values
+	- You do NOT need to produce the lines that say "Sample program output:" -or-
+	  "End of exercise - Press any key to continue . . .".
+
+	*/
+	// <write your solution code below this comment>
+
+
+
+	cout << "End of exercise - ";
+
+	return 0;
+}
+
+/*  Sample program output:
+
+Welcome to CSC 140!
+You will be learning Computer Science.
+
+In this course, you will use:
+C
++
++
+
+Attendance at Open Lab Days is required!
+Students in this course are NOT allowed to work together on Labs or MPL.
+Be sure to ask the instructor or tutors for help when you need it!
+
+End of exercise - Press any key to continue . . .
+
+*/
